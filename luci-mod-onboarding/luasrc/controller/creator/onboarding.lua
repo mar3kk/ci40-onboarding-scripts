3dafb6612c92aaabb07527d2f2601329890b8cc1
Marek Kubiczek <marek.kubiczek@imgtec.com>
2016-11-22 15:15:58 +0100

    Improve user experience of onboarding process
    Signed-off-by: Marek Kubiczek <marek.kubiczek@imgtec.com>

--- luci-mod-onboarding/luasrc/controller/creator/onboarding.lua
+++ luci-mod-onboarding/luasrc/controller/creator/onboarding.lua
@@ -16,13 +16,18 @@ function index()
     entry({"admin", "creator", "onboarding", "unprovision"}, call("unprovision"), nil, nil).dependent=false
     entry({"admin", "creator", "onboarding", "idp_login_completed"}, call("idp_login_completed"), nil, nil).dependent=false
     entry({"admin", "creator", "onboarding", "idp_login_completed2"}, call("idp_login_completed2"), nil, nil).dependent=false
+    entry({"admin", "creator", "onboarding", "is_board_connected_to_device_server"}, call("is_board_connected_to_device_server"), nil, nil).dependent=false
+
 end
 
 function onboarding_index()
-    if onboarding.isOnboardingCompleted() then
+    local isOnboardingCompleted = onboarding.isOnboardingCompleted();
+    if (not isOnboardingCompleted) then
+        luci.template.render("creator_onboarding/onboarding_info", {isProvisioned = onboarding.isOnboardingCompleted()})
+    elseif onboarding.isBoardConnectedToDeviceServer() then
         luci.template.render("creator_onboarding/onboarding_completed")
     else
-        luci.template.render("creator_onboarding/onboarding_info")
+        luci.template.render("creator_onboarding/onboarding_info", {isProvisioned = onboarding.isOnboardingCompleted()})
     end
 end
 
@@ -31,11 +36,11 @@ function idp_login()
     local url = "https://id.creatordev.io/oauth2/auth"
     local client_id = "2fcb0129-4354-4520-af54-8e3017b8e1f6"
     local scope = "core+openid+offline"
-    local redirectUri = "https://Ci40-mar3kk.local/cgi-bin/luci/admin/creator/onboarding/idp_login_completed"
+    local redirectUri = "https://OpenWrt.local/cgi-bin/luci/admin/creator/onboarding/idp_login_completed"
     local state = "dummy_state"
     local response_type = "id_token"
     local nonce = "ABCDEFGH"
-
+    --luci.http.write(url .. "?client_id=" .. client_id .. "&scope=" .. scope .. "&redirectUri=" .. redirectUri .. "&state=" .. state .. "&response_type=" .. response_type .. "&nonce=" .. nonce)
     luci.http.redirect(url .. "?client_id=" .. client_id .. "&scope=" .. scope .. "&redirectUri=" .. redirectUri .. "&state=" .. state .. "&response_type=" .. response_type .. "&nonce=" .. nonce)
 end
 
@@ -47,18 +52,18 @@ end
 
 -- Ajax functions --
 
-function start_onboarding()
-    local ds_url = tostring(luci.http.formvalue("ds_url"))
-    local key = tostring(luci.http.formvalue("key"))
-    local secret = tostring(luci.http.formvalue("secret"))
-    local name = tostring(luci.http.formvalue("secret"))
-    local ok, err = pcall(onboarding.doOnboarding, ds_url, key, secret, name)
-    if not ok then
-        luci.http.status(500, err.msg);
-    else
-        luci.http.status(200, "OK");
-    end
-end
+-- function start_onboarding()
+--     local ds_url = tostring(luci.http.formvalue("ds_url"))
+--     local key = tostring(luci.http.formvalue("key"))
+--     local secret = tostring(luci.http.formvalue("secret"))
+--     local name = tostring(luci.http.formvalue("secret"))
+--     local ok, err = pcall(onboarding.doOnboarding, ds_url, key, secret, name)
+--     if not ok then
+--         luci.http.status(500, err.msg);
+--     else
+--         luci.http.status(200, "OK");
+--     end
+-- end
 
 function start_onboarding()
     local ds_url = tostring(luci.http.formvalue("ds_url"))
@@ -66,11 +71,15 @@ function start_onboarding()
     local secret = tostring(luci.http.formvalue("secret"))
     local endPointName = tostring(luci.http.formvalue("endPointName"))
     local ok, err = pcall(onboarding.doOnboarding, ds_url, key, secret, endPointName)
-    if not ok then
-        luci.http.status(500, err.msg);
-    else
-        luci.http.status(200, endPointName);
+    local i = 0;
+    while (not onboarding.isBoardConnectedToDeviceServer()) do
+        os.execute("sleep " .. tonumber(1))
+        i=i+1
+        if (i > 10) then
+            luci.http.status(500, endPointName);
+        end
     end
+    luci.http.status(200, endPointName);
 end
 
 function unprovision()
@@ -96,6 +105,10 @@ function idp_login_completed2()
     luci.template.render("creator_onboarding/idp_login_completed", {key = key, secret = secret})
 end
 
-function isBoardConnectedToDeviceServer()
-    return ds.getClients()
+function is_board_connected_to_device_server()
+    if (onboarding.isBoardConnectedToDeviceServer()) then
+        luci.http.status(200, "OK")
+    else
+        luci.http.status(503, "Device Server unreachable")
+    end
 end
